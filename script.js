3647106cf8aba9026f1e45cd76c32cdcf35aabb8
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2022-01-21 12:11:10 +0100

    js update

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const dark= document.querySelector("div")
+console.log(dark);
 function openup(url) {
     console.log('url')
-    window.open(url, 'hallo','top=100',' left=6'," width=1000, height=1000");
+    window.open(url,' hallo' ,"top=100, left=100 , width=1100, height=550");
+    dark.classList.add("dark");
 }
+window.addEventListener("focus",function(){
+    console.log("focus")
+    dark.classList.remove("dark");
+});
