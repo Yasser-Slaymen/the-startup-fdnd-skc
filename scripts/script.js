19670748bd5a85139daa43d818ee4077066f0731
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2022-01-26 12:59:25 +0100

    js updading

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,23 @@
-const dark= document.querySelector("div")
-console.log(dark);
-function openup(url) {
-    console.log('url')
-    window.open(url,' hallo' ,"top=100, left=100 , width=1100, height=550");
+// const dark= document.querySelector("div")
+// console.log(dark);
+// function openup(url) {
+//     console.log('url')
+//     window.open(url,' hallo' ,"top=100, left=100 , width=1100, height=550");
     
-}
-window.addEventListener("focus",function(){
-    console.log("focus")
-    dark.classList.remove("dark");
-});
+// }
+// window.addEventListener("focus",function(){
+//     console.log("focus")
+//     dark.classList.remove("dark");
+// });
 // window.addEventListener("blur",function(){
 //      dark.classList.add("dark");
 
 //  })
+const btn=document.querySelectorAll('.studenten');
+var windowoObjectRefrence;
+var windowFeatures = "popup";
+
+btn.addEeventListener('onclick',function(){
+    console.log(url);
+   window.open("https://yasser-slaymen.github.io/ID-card/",' hallo' ,popup,"top=100, left=100 , width=1100, height=550")
+});
