8067ab626452862788567fbb72e15cb204d077f0
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2022-01-27 23:23:49 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
  
   const dark= document.querySelector("div")
   console.log(dark);
+  
   function openup(url) {
       console.log('url')
       window.open(url,' hallo' ,"top=100, left=100 , width=1100, height=550");
