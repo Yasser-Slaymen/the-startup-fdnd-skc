76dc58f3b253238ffd7c94ce61db401e501adfae
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2022-01-27 23:19:12 +0100

    js

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,18 @@
- const dark= document.querySelector("div")
- console.log(dark);
- function openup(url) {
-     console.log('url')
-     window.open(url,' hallo' ,"top=100, left=100 , width=1100, height=550");
+ 
+  const dark= document.querySelector("div")
+  console.log(dark);
+  function openup(url) {
+      console.log('url')
+      window.open(url,' hallo' ,"top=100, left=100 , width=1100, height=550");
     
- }
- window.addEventListener("focus",function(){
-     console.log("focus")
-     dark.classList.remove("dark");
- });
- window.addEventListener("blur",function(){
-      dark.classList.add("dark");
-
+  }
+  window.addEventListener("focus",function(){
+      console.log("focus")
+      dark.classList.remove("dark");
   });
-// const btn=document.querySelectorAll('.studenten');
-// btn.addEventListener('click', function(){
-//     alert('hallo');
-// });
+  window.addEventListener("blur",function(){
+       dark.classList.add("dark");
+
+   });
+
+
